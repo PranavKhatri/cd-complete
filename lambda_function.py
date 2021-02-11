fbf58e625fd4a88ba3faa7179311663b29f2aa61
PranavKhatri <khatripranav98@gmail.com>
2021-02-11 15:56:03 +0530

    label

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    print('Hello!')
+    print('Hello!!!!!!!!!!!!!!!!!!!!!!!!122')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
